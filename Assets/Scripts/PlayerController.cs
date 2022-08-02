38839ca52ddffc1c3aad359b3b472d7fdc653ee5
DiegoCasallas <master.drcd@gmail.com>
2022-08-02 08:57:37 -0500

    Already Jump animation and method jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,12 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidBody;
     //Var para identificar que elemtos son tipo suelo o plataforma
     public LayerMask groundMask;
+    //var para cargar las animaciones correspondientes
+    Animator animator;
+    /*traemos las variables creada en unity y las tipamos con const*/
+    /*variables en  MAYUCULAS convesion para constantes de mundo C++*/
+    private string STATE_ALIVE="isAlive";
+    private string STATE_ON_THE_GROUND="isOnTheGround";
 
 
 
@@ -19,7 +25,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        animator.SetBool(STATE_ALIVE,true);
+        animator.SetBool(STATE_ON_THE_GROUND,true);
     }
     // Update is called once per frame. 
     /*Se ejecuta hasta 60xseg*/
@@ -30,6 +37,14 @@ public class PlayerController : MonoBehaviour
         {
             Jump();
         }
+
+        animator.SetBool(STATE_ON_THE_GROUND,IsTouchingTheGround());
+
+        Debug.DrawRay(
+            /*Desde donde sale el rayo del 
+            personaje(ventro personaje*/this.transform.position,
+            /*dir rayo * distancia del rayo(1.5mts)*/Vector2.down*1.5f,
+            /*color rayo*/Color.red);
     }
 
     /*Awake sirve para despertar los objetos y cosas del juego antes 
@@ -37,6 +52,7 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
 
     }
     //Metodo para que salete mi personaje
@@ -49,7 +65,7 @@ public class PlayerController : MonoBehaviour
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
-    //Metodo que me dice si el personaje est· tocando el suelo(Ground)
+    //Metodo que me dice si el personaje est√° tocando el suelo(Ground)
     bool IsTouchingTheGround()
     {
         //Physics objeto que tiene un metodo(Raycast) que dispara rayos invisibles
@@ -60,10 +76,14 @@ public class PlayerController : MonoBehaviour
              el centro del personaje (2mts)*/2f,
             /*contra que debe chocar el rayo(Ground)*/groundMask))
         {
+            /*para que contifue la animacion*/
+            //animator.enabled=true;
             return true;
         }
         else 
         {
+            /*pausamos la aunimacion*/
+            //animator.enabled=false;
             return false;
         }
     }
