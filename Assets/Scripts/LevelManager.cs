a828f68e241c6658ca1d5b3478064f3f40a01be8
DiegoCasallas <master.drcd@gmail.com>
2022-09-01 15:36:06 -0500

    logica metodo addLevelBlock que agrega un bloque de nivel a

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,18 +12,18 @@ public class LevelManager : MonoBehaviour
     public static LevelManager sharedInstance;
 
     //Listas que contendrán los bloque de nivel (LevelBlock)
-    public List<LevelBlock>allTheLevelBlocks= new List<LevelBlock>();
-    public List<LevelBlock>currentLevelBlocks= new List<LevelBlock>();
-   
-    public Transform initialPosition;
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+    public List<LevelBlock> currentLevelBlocks = new List<LevelBlock>();
+
+    public Transform levelStartPosition;
     void Awake()
     {
         if (sharedInstance == null)
         {
-           sharedInstance=this;
+            sharedInstance = this;
         }
     }
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,8 +37,51 @@ public class LevelManager : MonoBehaviour
     {
 
     }
+
     public void AddLevelBlock()
     {
+        /*Agregar un nuevo bloque Aleatorio*/
+        int randomIdx = Random.Range(0, allTheLevelBlocks.Count);
+
+        LevelBlock block;
+
+        Vector3 spawnPosition = Vector3.zero;
+
+        /* Si no se ha añadido ningun bloque, agregamos el bloque que
+         esta dentro de all the levelblocks[0] */
+        if (currentLevelBlocks.Count == 0)
+        {
+            block = Instantiate(allTheLevelBlocks[0]);
+            spawnPosition = levelStartPosition.position;
+
+        }
+        else
+        {
+            block = Instantiate(allTheLevelBlocks[randomIdx]);
+            spawnPosition = currentLevelBlocks
+            [currentLevelBlocks.Count - 1].exitPoint.position;
+        }
+
+
+       /* Establecer el padre del bloque en la transformación del LevelManager. */
+       
+        block.transform.SetParent(this.transform, false);
+
+
+        /*hacer conincidir el punto de salida de uno con el punto de entrada del otro
+        en principio concviven en un espacio diferente, similar al cambio de ubicacion usando el plano cartesiano con ejes x,y
+        restamos el punto de */
+        Vector3 correction = new Vector3(
+            /* eje x */
+            spawnPosition.x - block.starPoint.position.x,
+            /*eje y*/
+            spawnPosition.y - block.starPoint.position.y,
+            0
+        );
+
+        block.transform.position = correction;
+        /*agremaos a la lista el objeto LevelBlcck block*/
+        currentLevelBlocks.Add(block);
 
     }
     public void RemoveLevelBlock()
